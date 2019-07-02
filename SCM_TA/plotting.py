e9479c63243fffb3bade0c3de068838ab4fae3e1
vahid133 <vetemadi87@gmail.com>
2019-07-02 16:14:38 +0430

    Code added to be able to plot the paretos

--- SCM_TA/plotting.py
+++ SCM_TA/plotting.py
@@ -3,18 +3,22 @@ import pandas as pd
 import os
 from os.path import dirname, abspath
 from pathlib import Path
+import sys
 
 '''
 d=dirname(dirname(abspath('ploting.py')))
 d=d+'/paretoFront'
 '''
 
-os.chdir('C:/Users/DistLab3/git/SCM_TA/SCM_TA/archives/1/JDT/JDTMilestone3.1.0/KRRGZ')
-for filename in os.listdir('.'):
+#os.chdir('C:/Users/DistLab3/git/SCM_TA/SCM_TA/archives/1/JDT/JDTMilestone3.1.1/SD')
+JDT_fileNames=['JDTMilestone3.1.0', 'JDTMilestone3.1.1', 'JDTMilestone3.1.2', 'JDTMilestoneM1','JDTMilestoneM2', 'JDTMilestoneM3'
+				,'JDTMilestoneM4', 'JDTMilestoneM5', 'JDTMilestoneM6']
+
+for filename in os.listdir(os.getcwd()+'\\archives\\1\\'+sys.argv[1]+'\\'+sys.argv[2]+'\\'+sys.argv[3]):
 	if(filename.endswith('.csv')):
 		print(filename)
-		df=pd.read_csv(filename)
+		df=pd.read_csv(os.getcwd()+'\\archives\\1\\'+sys.argv[1]+'\\'+sys.argv[2]+'\\'+sys.argv[3]+'\\'+filename)
 		print(df)
 		df.plot(kind='scatter', x='Time', y='Cost', title=os.path.splitext(filename)[0])
-		plt.show()
+		plt.savefig(os.getcwd()+'\\archives\\1\\'+sys.argv[1]+'\\'+sys.argv[2]+'\\'+sys.argv[3]+'\\'+os.path.splitext(filename)[0]+'.png')
 	
