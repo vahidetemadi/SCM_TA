386c3e7f22e5c131b0e3f89d18b3f5c65db8be27
vahid133 <vetemadi87@gmail.com>
2019-07-18 19:58:06 +0430

    updating automation behind stat test

--- SCM_TA/loadFromYaML.py
+++ SCM_TA/loadFromYaML.py
@@ -2,21 +2,65 @@ import yaml
 import os
 import sys
 import pandas as pd
+from scipy.stats import wilcoxon
 
-listOfDataFrames=[]
 
-for filename in os.listdir(os.path.join(os.getcwd(),"results",sys.argv[1])):
-	if(filename.endswith('.yaml')):
-		data=yaml.load(open(os.path.join(os.getcwd(),"results",sys.argv[1],filename)))
-		name=os.path.splitext(filename)[0].split('_')
-		if name[2]=='1' and name[3]=='1':
-			name[1]=pd.DataFrame(pd.np.empty((0, 8)))
-			name[1].columns=["NSGAIIITA_Hypervolume", "NSGAIIITA_GenerationalDistance", "NSGAIIITA_Spacing", "NSGAIIITA_Contribution",
-							"KRRGZ_Hypervolume", "KRRGZ_GenerationalDistance","KRRGZ_Spacing", "KRRGZ_Contribution"]
-			listOfDataFrames.append(name[1])
+class readResults:
+	@staticmethod
+	def loadDataIntoDataFrames(args):
+		dictOfDataFrames={}
+		for filename in os.listdir(os.path.join(os.getcwd(),"results",args[1])):
+			if(filename.endswith('.yaml')):
+				data=yaml.load(open(os.path.join(os.getcwd(),"results",args[1],filename)))
+				name=os.path.splitext(filename)[0].split('_')
+				milestoneName=name[1]
+				if milestoneName not in dictOfDataFrames:
+					name[1]=pd.DataFrame(pd.np.empty((0, 8)))
+					name[1].columns=["NSGAIIITA_Hypervolume", "NSGAIIITA_GenerationalDistance", "NSGAIIITA_Spacing", "NSGAIIITA_Contribution",
+									"KRRGZ_Hypervolume", "KRRGZ_GenerationalDistance","KRRGZ_Spacing", "KRRGZ_Contribution"]
+					dictOfDataFrames.update({milestoneName:name[1]})
 
-		index=listOfDataFrames.index(name[1])
-		listOfDataFrames[index].loc[listOfDataFrames[index].shape[0]+1]=[data['NSGAIIITA']['Hypervolume'],data['NSGAIIITA']['GenerationalDistance'], data['NSGAIIITA']['Spacing'], data['NSGAIIITA']['Contribution']
-																		 , data['KRRGZ']['Hypervolume'],data['KRRGZ']['GenerationalDistance'], data['KRRGZ']['Spacing'], data['KRRGZ']['Contribution'] ]
+				dictOfDataFrames.get(milestoneName).loc[dictOfDataFrames.get(milestoneName).shape[0]+1]=[data['NSGAIIITA']['Hypervolume'],data['NSGAIIITA']['GenerationalDistance'], data['NSGAIIITA']['Spacing'], data['NSGAIIITA']['Contribution']
+																				 , data['KRRGZ']['Hypervolume'],data['KRRGZ']['GenerationalDistance'], data['KRRGZ']['Spacing'], data['KRRGZ']['Contribution'] ]
 
-		print(listOfDataFrames.index(name[1]))
+				print(dictOfDataFrames.get(milestoneName))
+		return dictOfDataFrames	
+
+	@staticmethod
+	def storeDataIntoCSV(dictOfDataFrames):		
+		for key, value in dictOfDataFrames.items():
+			value.to_csv(os.path.join(os.getcwd(),"results",sys.argv[1], key+'.csv'))
+
+
+class statitisticalTest:
+	@staticmethod
+	def getWilcoxonTest(dataframe, indicator):
+		return wilcoxon(dataframe['NSGAIIITA_'+indicator], dataframe['KRRGZ_'+indicator]);
+
+
+
+
+loadResults=readResults()
+dictOfDataFrames=loadResults.loadDataIntoDataFrames(sys.argv)
+loadResults.storeDataIntoCSV(dictOfDataFrames)
+
+statTest=statitisticalTest()
+dictOfStatTestDataFrame={}
+for key, value in dictOfDataFrames.items():
+	dataframeName=str('wilcoxonDataframeResults_'+key)
+	dataframeName=pd.DataFrame(pd.np.empty((4, 2)), index=["Hypervolume", "GenerationalDistance", "Spacing", "Contribution"],
+								columns=["wilcoxonTestResult", "p-value"] )
+
+	for i, row in dataframeName.iterrows():
+		result=statTest.getWilcoxonTest(dataframeName,i)
+		dataframeName.loc[i]=result
+
+	dictOfStatTestDataFrame.update({key:dataframeName})
+
+
+
+
+
+
+
+#
