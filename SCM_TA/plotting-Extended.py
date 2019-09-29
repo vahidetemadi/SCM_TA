aca00c7f63a713f49a52c0708025f88c43db210a
vahid133 <vetemadi87@gmail.com>
2019-09-29 21:28:51 +0330

    Just updated python files to include effect size computation

--- SCM_TA/plotting-Extended.py
+++ SCM_TA/plotting-Extended.py
@@ -16,25 +16,24 @@ JDT_fileNames=['JDTMilestone3.1.0', 'JDTMilestone3.1.1', 'JDTMilestone3.1.2', 'J
 
 NEF=['50000','100000', '150000', '200000', '250000']
 
-for filename in os.listdir(os.getcwd()+'\\archives\\'+sys.argv[1]+'\\'+sys.argv[2]+'\\'+sys.argv[3]+'\\'+sys.argv[4]):
+for filename in os.listdir(os.path.join(os.getcwd(),'archives',sys.argv[1],sys.argv[2],sys.argv[3],sys.argv[4])):
 	if(filename.endswith('.csv')):
 		print(filename)
-		df=pd.read_csv(os.getcwd()+'\\archives\\'+sys.argv[1]+'\\'+sys.argv[2]+'\\'+sys.argv[3]+'\\'+sys.argv[4]+'\\'+filename)
+		df=pd.read_csv(os.path.join(os.getcwd(),'archives',sys.argv[1],sys.argv[2],sys.argv[3],sys.argv[4],filename))
 		print(df)
 		name=os.path.splitext(filename)[0].split('_')
 		if(sys.argv[4]=='KRRGZ'):
-			df2=pd.read_csv(os.getcwd()+'\\archives\\'+sys.argv[1]+'\\'+sys.argv[2]+'\\'+sys.argv[3]+
-				'\\SD\\'+name[0]+'_'+name[1]+'_SD_'+name[3]+os.path.splitext(filename)[1])
-			df3=pd.read_csv(os.getcwd()+'\\archives\\'+sys.argv[1]+'\\'+sys.argv[2]+'\\'+sys.argv[3]+
-				'\\RS\\'+name[0]+'_'+name[1]+'_RS_'+name[3]+os.path.splitext(filename)[1])
+			df2=pd.read_csv(os.path.join(os.getcwd(),'archives',sys.argv[1],sys.argv[2],sys.argv[3],
+				'SD',name[0]+'_'+name[1]+'_SD_'+name[3]+os.path.splitext(filename)[1]))
+			df3=pd.read_csv(os.path.join(os.getcwd(),'archives',sys.argv[1],sys.argv[2],sys.argv[3],
+				'RS',name[0]+'_'+name[1]+'_RS_'+name[3]+os.path.splitext(filename)[1]))
 		else:
-			df2=pd.read_csv(os.getcwd()+'\\archives\\'+sys.argv[1]+'\\'+sys.argv[2]+'\\'+sys.argv[3]+
-				'\\KRRGZ\\'+name[0]+'_'+name[1]+'_KRRGZ_'+name[3]+os.path.splitext(filename)[1])
-			df3=pd.read_csv(os.getcwd()+'\\archives\\'+sys.argv[1]+'\\'+sys.argv[2]+'\\'+sys.argv[3]+
-				'\\RS\\'+name[0]+'_'+name[1]+'_RS_'+name[3]+os.path.splitext(filename)[1])
+			df2=pd.read_csv(os.path.join(os.getcwd(),'archives',sys.argv[1],sys.argv[2],sys.argv[3],
+				'KRRGZ',name[0]+'_'+name[1]+'_KRRGZ_'+name[3]+os.path.splitext(filename)[1]))
+			df3=pd.read_csv(os.path.join(os.getcwd(),'archives',sys.argv[1],sys.argv[2],sys.argv[3],
+				'RS',name[0]+'_'+name[1]+'_RS_'+name[3]+os.path.splitext(filename)[1]))
 
 		ax1=df.plot(kind='scatter', x='Time', y='Cost', title=name[0]+'_'+name[1]+'_'+name[3], c='blue', s=30)
 		ax2=df2.plot(kind='scatter', x='Time', y='Cost', title=name[0]+'_'+name[1]+'_'+name[3], c='green',marker='^', ax=ax1, s=30)
 		df3.plot(kind='scatter', x='Time',c='red' ,y='Cost', title=name[0]+'_'+name[1]+'_'+name[3], marker='s', ax=ax2, s=30)	
-		plt.savefig(os.getcwd()+'\\archives\\'+sys.argv[1]+'\\'+sys.argv[2]+'\\'+sys.argv[3]+'\\'+name[0]+'_'+name[1]+'_'+name[3]+'.png')
-	
+		plt.savefig(os.path.join(os.getcwd(),'archives',sys.argv[1],sys.argv[2],sys.argv[3],name[0]+'_'+name[1]+'_'+name[3]+'.png'))
