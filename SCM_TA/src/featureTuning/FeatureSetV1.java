51bcada8d686928d7ab04f9b52045d154142b2fe
vahidetemadi <vetemadi87@gmail.com>
2020-01-04 12:04:36 +0330

    just changed a bit the feature vector 1

--- SCM_TA/src/featureTuning/FeatureSetV1.java
+++ SCM_TA/src/featureTuning/FeatureSetV1.java
@@ -1,23 +1,30 @@
 package featureTuning;
 
 import org.moeaframework.core.Solution;
+
 import java.util.HashMap;
+import java.util.Map;
 
 
 public class FeatureSetV1 implements FeatureGeneration {
 	
 	static int numberOfVariable;
 	static HashMap<Integer, String> featureVectorIndex=new HashMap<Integer, String>();
-			
+	{
+		featureVectorIndex.put(1, "numOfDevs");
+		
+	}
 	@Override
 	/*
 	 * Takes a solution and initialize that with the variable
-	 * 
+	 * @ 
 	 * 
 	 * @see featureTuning.FeatureGeneration#getTheFeatureVector(org.moeaframework.core.Solution)
 	 */
 	public Solution getTheFeatureVector(Solution solution) {
-		solution.setVariable(index, variable);
+		for(Map.Entry<Integer, String> varaible:featureVectorIndex)
+			solution.setVariable(index, variable);
+			
 		return solution;
 	}
 
