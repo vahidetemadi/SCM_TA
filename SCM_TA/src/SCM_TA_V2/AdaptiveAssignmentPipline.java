5c78034859a9cc59d7291c73ba09b88a2ff9012e
vahidetemadi <vetemadi87@gmail.com>
2020-01-05 00:38:11 +0330

    just added the missed file

--- SCM_TA/src/SCM_TA_V2/AdaptiveAssignmentPipline.java
+++ SCM_TA/src/SCM_TA_V2/AdaptiveAssignmentPipline.java
@@ -14,6 +14,7 @@ import java.util.Random;
 import java.util.Scanner;
 
 import org.jgrapht.alg.color.SaturationDegreeColoring;
+import org.moeaframework.core.Solution;
 
 import smile.sequence.HMM;
 import SCM_TA_V1.*;
